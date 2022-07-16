6531f10a4f0f1ffb6279074bc3493867edef7772
Charinda <charinda@calcey.com>
2022-07-16 17:45:45 +0530

    Add project structure

---
+++ src/modules/home/screens/Home.tsx
@@ -0,0 +1,23 @@
+// import { AppIcon } from '@src/components';
+// import icons from '@src/theme/icons';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Entype from 'react-native-vector-icons/Entypo';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const Home: React.FC = () => (
+  <View style={styles.container}>
+    <Text>Home</Text>
+    {/* <AppIcon name={icons.book} /> */}
+    <Entype name="home" size={24} />
+  </View>
+);
+
+export default Home;
